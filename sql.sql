da873cba8f8578e4bc46376576451a2da92d04c3
Gregory Daynes <gregdaynes@gmail.com>
2011-06-28 16:59:54 -0700

    DatabaseTableZones
    added behaviors to table
    
    updated sql for manual install

--- sql.sql
+++ sql.sql
@@ -12,3 +12,20 @@ CREATE TABLE `#__adverts_sites` (
   `checked_out` int(11) unsigned NOT NULL DEFAULT '0',
   `checked_out_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
 );
+
+CREATE TABLE `#__adverts_zones` (
+  `adverts_zone_id` SERIAL,
+  `name` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `size` int(11) NOT NULL DEFAULT '-1',
+  `notes` mediumtext NOT NULL,
+  `width` int(11) NOT NULL,
+  `height` int(11) NOT NULL,
+  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `enabled` tinyint(1) NOT NULL,
+  `site_id` int(11) NOT NULL,
+  `chained_zone_id` int(11) NOT NULL,
+  `version` int(11) unsigned NOT NULL DEFAULT '1',
+  `checked_out` int(11) unsigned NOT NULL DEFAULT '0',
+  `checked_out_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00'
+);
