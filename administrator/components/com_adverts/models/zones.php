52457c9ea4dc429cf75fc823a58f5e5726787a55
gregdaynes <gregdaynes@gmail.com>
2011-06-28 23:01:32 -0700

    Playing with the zones view filters
    rough addition of "has banners" and "empty" zone filters.
    They work, but they only take into consideration campaigns that are assigned to zones. Not the campaigns publish status. Also the query is slow - the zones field will need to be moved into it's own table.

--- administrator/components/com_adverts/models/zones.php
+++ administrator/components/com_adverts/models/zones.php
@@ -9,9 +9,21 @@ class ComAdvertsModelZones extends ComDefaultModelDefault
         parent::__construct($config);
         
         $this->_state
-            ->insert('enabled', 'int')
-            ->insert('website',	'int')
+            ->insert('enabled',		'int')
+            ->insert('website',		'int')
+            ->insert('populated',	'int')
             ;
+    }
+    
+    protected function _buildQueryJoins(KDatabaseQuery $query)
+    {
+    	parent::_buildQueryJoins($query);
+    	
+    	$state = $this->_state;
+    	
+    	if (is_numeric($state->populated)) {
+	    	$query->join('LEFT', 'adverts_campaigns AS campaign', 'FIND_IN_SET(tbl.adverts_zone_id,campaign.zones) > 0');
+    	}
     }
     
     protected function _buildQueryWhere(KDatabaseQuery $query)
@@ -27,5 +39,23 @@ class ComAdvertsModelZones extends ComDefaultModelDefault
         if (is_numeric($state->website)) {
         	$query->where('tbl.site_id', '=', $state->website);
         }
+        
+        if (is_numeric($state->populated)) {
+	        $tmp = 'IS NULL';
+	        
+	        if ($state->populated == 1) {
+	        	$tmp = 'IS NOT NULL';
+	       	}
+	   
+	        $query->where('campaign.zones', $tmp);
+	        
+	   }
+    }
+    
+    protected function _buildQueryGroup(KDatabaseQuery $query)
+    {
+    	parent::_buildQueryGroup($query);
+    	
+    	$query->group('tbl.adverts_zone_id');
     }
 }
--- administrator/components/com_adverts/views/zones/tmpl/default_filter.php
+++ administrator/components/com_adverts/views/zones/tmpl/default_filter.php
@@ -4,21 +4,33 @@ defined('KOOWA') or die( 'Restricted access' ); ?>
 
 <div id="filter" class="group">
 	<ul>
-		<li class="<?= !is_numeric($state->enabled) ? 'active' : ''; ?> separator-right">
-			<a href="<?= @route('enabled=' ) ?>">
+		<li class="<?= !is_numeric($state->enabled) && !is_numeric($state->populated) ? 'active' : ''; ?> separator-right">
+			<a href="<?= @route('enabled=&populated=' ) ?>">
 			    <?= @text('All') ?>
 			</a>
-		</li>              
+		</li>  
+		            
 		<li class="<?= $state->enabled == '1' ? 'active' : ''; ?>">
 			<a href="<?= @route('enabled=1' ) ?>">
 			    <?= @text('Published') ?>
 			</a> 
 		</li>
-		<li class="<?= $state->enabled == '0' ? 'active' : ''; ?>">
+		<li class="<?= $state->enabled == '0' ? 'active' : ''; ?> separator-right">
 			<a href="<?= @route('enabled=0' ) ?>">
 			    <?= @text('Unpublished') ?>
 			</a> 
 		</li>
-
+		
+		<li class="<?= $state->populated == '1' ? 'active' : ''; ?>">
+			<a href="<?= @route('populated=1' ) ?>">
+			    <?= @text('Has Banners') ?>
+			</a> 
+		</li>
+		<li class="<?= $state->populated == '0' ? 'active' : ''; ?> separator-right">
+			<a href="<?= @route('populated=0' ) ?>">
+			    <?= @text('Empty') ?>
+			</a> 
+		</li>
+		
 	</ul>
 </div>
