bf3f741e3aaf6a457353c33bc49b5f5e6fc81939
Gregory Daynes <gregdaynes@gmail.com>
2011-06-29 13:17:54 -0700

    zones view filters
    fixed populated filters to work as they should, using the relational db correctly too.
    I'm so happy! took too long.

--- administrator/components/com_adverts/models/zones.php
+++ administrator/components/com_adverts/models/zones.php
@@ -4,58 +4,89 @@ defined('KOOWA') or die('Restricted access');
 
 class ComAdvertsModelZones extends ComDefaultModelDefault
 {
-    public function __construct(KConfig $config)
-    {
-        parent::__construct($config);
-        
-        $this->_state
-            ->insert('enabled',		'int')
-            ->insert('website',		'int')
-            ->insert('populated',	'int')
-            ;
-    }
-    
-    protected function _buildQueryJoins(KDatabaseQuery $query)
-    {
-    	parent::_buildQueryJoins($query);
-    	
-    	$state = $this->_state;
-    	
-    	if (is_numeric($state->populated)) {
-	    	$query->join('LEFT', 'adverts_campaigns AS campaign', 'FIND_IN_SET(tbl.adverts_zone_id,campaign.zones) > 0');
-    	}
-    }
-    
-    protected function _buildQueryWhere(KDatabaseQuery $query)
-    {
-        parent::_buildQueryWhere($query);
-        
-        $state = $this->_state;
-        
-        if (is_numeric($state->enabled)) {
-            $query->where('tbl.enabled', '=', $state->enabled);
-        }
-        
-        if (is_numeric($state->website)) {
-        	$query->where('tbl.site_id', '=', $state->website);
-        }
-        
-        if (is_numeric($state->populated)) {
-	        $tmp = 'IS NULL';
-	        
-	        if ($state->populated == 1) {
-	        	$tmp = 'IS NOT NULL';
-	       	}
-	   
-	        $query->where('campaign.zones', $tmp);
-	        
-	   }
-    }
-    
-    protected function _buildQueryGroup(KDatabaseQuery $query)
-    {
-    	parent::_buildQueryGroup($query);
-    	
-    	$query->group('tbl.adverts_zone_id');
-    }
+	public function __construct(KConfig $config)
+	{
+		parent::__construct($config);
+		
+		$this->_state
+			->insert('enabled',		'int')
+			->insert('website',		'int')
+			->insert('populated',	'int')
+			;
+	}
+	
+	protected function _buildQueryColumns(KDatabaseQuery $query)
+	{
+		parent::_buildQueryColumns($query);
+		
+		$state = $this->_state;
+		
+		if (is_numeric($state->populated)) {
+			$query
+				->select('GROUP_CONCAT(campaign.adverts_campaign_id) AS campaigns')
+				->select('SUM(campaign.enabled) AS campaign_state')
+				;
+		}
+	}
+	
+	protected function _buildQueryJoins(KDatabaseQuery $query)
+	{
+		parent::_buildQueryJoins($query);
+		
+		$state = $this->_state;
+		
+		if (is_numeric($state->populated)) {
+			$query
+				->join('LEFT', 'adverts_campaign_zones AS cz', 'cz.zid = tbl.adverts_zone_id')
+				->join('LEFT', 'adverts_campaigns AS campaign', 'cz.cid = campaign.adverts_campaign_id')
+				;
+		}
+	}
+	
+	protected function _buildQueryWhere(KDatabaseQuery $query)
+	{
+		parent::_buildQueryWhere($query);
+		
+		$state = $this->_state;
+		
+		if (is_numeric($state->enabled)) {
+			$query->where('tbl.enabled', '=', $state->enabled);
+		}
+		
+		if (is_numeric($state->website)) {
+			$query->where('tbl.site_id', '=', $state->website);
+		}
+		
+		if (is_numeric($state->populated)) {
+				
+		}
+	}
+	
+	protected function _buildQueryGroup(KDatabaseQuery $query)
+	{
+		parent::_buildQueryGroup($query);
+		
+		$query->group('tbl.adverts_zone_id');
+
+	}
+	
+	protected function _buildQueryHaving(KDatabaseQuery $query)
+	{
+		parent::_buildQueryHaving($query);
+		
+		$state = $this->_state;
+		
+		if (is_numeric($state->populated)) {
+			if($state->populated == 1) {
+				$query
+					->having('campaign_state > 0')
+					;
+			} else {
+				$query
+					->having('campaign_state = 0 OR campaigns IS NULL')
+					;
+			}
+		}
+
+	}
 }
