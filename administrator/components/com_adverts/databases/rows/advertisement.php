0405db8668e767475f58912ad25437d36df82908
Gregory Daynes <gregdaynes@gmail.com>
2011-07-05 09:02:54 -0700

    File Upload
    starting to add file uploading.
    This way does not work, but I may come back to it.
    Going to try and implement com_files reusable method.

--- administrator/components/com_adverts/databases/rows/advertisement.php
+++ administrator/components/com_adverts/databases/rows/advertisement.php
@@ -32,8 +32,7 @@ class ComAdvertsDatabaseRowAdvertisement extends KDatabaseRowDefault
 			}
 		}
 			
-		if (in_array('file_url', $modified))
-		{
+		
 			$file = KRequest::get('files.file_url', 'filename');
 			
 			if ($file['name'] != '')
@@ -41,8 +40,33 @@ class ComAdvertsDatabaseRowAdvertisement extends KDatabaseRowDefault
 				jimport('joomla.filesystem.file');
 				jimport('joomla.filesystem.folder');
 				
+				// get filename. make safe. make lowercase
+				$filename = strtolower(JFile::makeSafe($file['name']));
+				
+				
+				/**
+				 * folder
+				 * 
+				 * folder for storing the banners uploaded
+				 *
+				 * @todo adapt for parameters
+				 */
+				$pathBase = JPATH_SITE.DS.'images'.DS.'adverts';
+				// if the folder does not exist, create folder
+				if (!JFolder::exists($pathBase)) { 
+					JFolder::create($pathBase, 0775);
+				}
+				
+				// src file to move
+				$src = $file['tmp_name'];
+				// destination + filename + extension
+				$dest = $pathBase.DS.$filename;
+	
+				file_put_contents($dest, $file);
+				
+				$this->file_url = $dest;
 			}
-		}
+
 		
 		return (bool) $result;
 	}
