5f14bc4648ade2277528bdb49c05727e08b9b513
Gregory Daynes <gregdaynes@gmail.com>
2011-07-14 11:37:13 -0700

    clickTag support
    Well… that was easy. Almost too easy. I'm kinda worried I did it wrong. But it works in simple tests :\

--- modules/mod_adverts/tmpl/default_flash.php
+++ modules/mod_adverts/tmpl/default_flash.php
@@ -7,13 +7,14 @@ defined('KOOWA') or die('Restricted access'); ?>
 		var adSwiff = new Swiff('/media/com_adverts/attachments/<?= @escape($advertisement->primary_file); ?>', {
 			container: $('advert_<?= $unique; ?>'),
 			width: 300,
-			height: 250
+			height: 250,
+			vars: {clickTag: '<?= @route($advertisement->click_url); ?>'}
 		});
 	});
 </script>
 
 <noscript>
-	<a href="<?=@route('view=advertisement&id='.$advertisement->id)?>" title="<?= $advertisement->name ?>">
+	<a href="<?=@route($advertisement->click_url)?>" title="<?= $advertisement->name ?>">
 		<img src="/media/com_adverts/attachments/<?= $advertisement->alternative_file ?>" alt="<?= $advertisement->alt_text ?>"/>
 	</a>
 </noscript>
